789e96814e7826fce048317f39c1eff52e3140b2
tlcks <tlcks@DESKTOP-SGLQ3GS>
2020-03-19 21:32:37 +0900

    Create a menu

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,12 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		System.out.println("I am a new Java programmer!");
+		System.out.println("*** Account Management System Menu ***");
+		System.out.println(" 1. Add Account");
+		System.out.println(" 2. Delete Account");
+		System.out.println(" 3. Edit Account");
+		System.out.println(" 4. View Account");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one number between 1 - 5:");
 	}
 }
