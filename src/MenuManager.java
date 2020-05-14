d75888f31a1a685c51a506785be122eafe7ea5d2
tlcks <tlcks@DESKTOP-SGLQ3GS>
2020-05-15 00:21:06 +0900

    Add Exception

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,33 +1,41 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuManager {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		AccountManager accountManager = new AccountManager(input);
-		
+
 		int num = -1;
 		while (num != 5) {
-			showMenu();
-			num = input.nextInt();
-			switch(num) {
-			case 1:
-				accountManager.addAccount();
+			try {
+				showMenu();
+				num = input.nextInt();
+				switch(num) {
+				case 1:
+					accountManager.addAccount();
+					break;
+				case 2:
+					accountManager.deleteAccount();
+					break;
+				case 3:
+					accountManager.editAccount();
+					break;
+				case 4:
+					accountManager.viewAccounts();
+					break;
+				default:
+					continue;
+				}
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Please insert number.");
 				break;
-			case 2:
-				accountManager.deleteAccount();
-				break;
-			case 3:
-				accountManager.editAccount();
-				break;
-			case 4:
-				accountManager.viewAccounts();
-				break;
-			default:
-				continue;
 			}
 		}
+
 	}
-	
+
 	public static void showMenu() {
 		System.out.println("*** Account Management System Menu ***");
 		System.out.println(" 1. Add Account");
